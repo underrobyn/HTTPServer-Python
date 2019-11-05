fd8965bf663b6957b318d86eabd65c006dac1ca3
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2019-11-05 00:12:49 +0000

    Testing applying CLI arguments

--- lib/settings.py
+++ lib/settings.py
@@ -1,24 +1,52 @@
 from json import loads
+from sys import argv
 from lib.utils import *
 from lib.content import FileServe
 
+arguments = {}
 config = {}
-file = "_config.json"
+config_file = "_config.json"
 
-if not check_file_exists(file):
-	print("Could not find _config.json")
-	exit(99)
+def parse_cli_args():
+	global arguments
 
-if not check_can_read(file):
-	print("Unable to read _config.json")
-	exit(99)
+	if len(argv) < 2: return
 
-with open(file,"r") as setting_file:
-	print("Config file read from file.")
-	config = loads(setting_file.read())
+	for i in argv:
+		if "=" not in i: continue
+
+		tokenised = i.split("=")
+		if tokenised[0] == "" or tokenised[1] == "": continue
+
+		arguments[tokenised[0]] = tokenised[1]
+
+	apply_cli_args()
+
+def apply_cli_args():
+	global arguments, config_file
+
+	if "config" in arguments:
+		config_file = arguments["config"]
+
+def load_config():
+	global config, config_file
+
+	if not check_file_exists(config_file):
+		print("Could not find _config.json")
+		exit(99)
+
+	if not check_can_read(config_file):
+		print("Unable to read _config.json")
+		exit(99)
+
+	with open(config_file, "r") as setting_file:
+		print("Config file read from file.")
+		config = loads(setting_file.read())
+
+parse_cli_args()
+load_config()
 
 # Console logging
-
 def log(level, message):
 	if level >= config["console"]["log_level"]:
 			print(message)
